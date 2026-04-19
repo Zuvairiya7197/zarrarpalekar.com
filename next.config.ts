d6952ace4b5546ec083bd0c1ee3ccaf676595311
Zarrar Palekar <zarrar@live.in>
2026-04-19 15:35:21 +0530

    javascript minified

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
-  productionBrowserSourceMaps: true,
+  productionBrowserSourceMaps: false,
   images: {
     formats: ["image/avif", "image/webp"],
     remotePatterns: [
